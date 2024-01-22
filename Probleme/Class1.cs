21b9a86ff029ff3eef186a5272e3247fea107b80
Matei stefanita <stefanita.matei@emanuel.ro>
2024-01-22 10:26:44 +0200

    problema 1 nu stiu cat de bine este

---
+++ Probleme/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Person
+{
+    public string FirstName { get; }
+    public string LastName { get; }
+    public int YearOfBirth { get; }
+    public int Age => DateTime.Now.Year - YearOfBirth;
+
+    public Person(string firstName, string lastName, int yearOfBirth)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        YearOfBirth = yearOfBirth;
+    }
+}
---
+++ Probleme/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Probleme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduceti numarul de persoane: ");
+            int numarPersoane = int.Parse(Console.ReadLine());
+
+            Person[] persoane = new Person[numarPersoane];
+
+            for (int i = 0; i < numarPersoane; i++)
+            {
+                Console.WriteLine($"Introduceti informatiile pentru persoana {i + 1}:");
+
+                Console.Write("Prenume: ");
+                string prenume = Console.ReadLine();
+
+                Console.Write("Nume de familie: ");
+                string numeDeFamilie = Console.ReadLine();
+
+                Console.Write("Anul nasterii: ");
+                int anulNasterii = int.Parse(Console.ReadLine());
+
+                persoane[i] = new Person(prenume, numeDeFamilie, anulNasterii);
+            }
+
+            Console.WriteLine("Informatiile persoanelor:");
+        }
+    }
+}
